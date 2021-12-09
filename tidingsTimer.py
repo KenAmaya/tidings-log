34c33018f2a2742308a0b3db620413cf3045dac4
KenAmaya <ken.amaya@protonmail.ch>
2021-12-10 12:14:19 +1300

    Complete parse_input() function

--- tidingsTimer.py
+++ tidingsTimer.py
@@ -4,18 +4,20 @@ from datetime import time as Time
 def parse_input(input_time):
     time_strings = input_time.split(":")
     time_chunks = []
-    time_format = {2:0, 1:0, 0:0} # 3 for hours, 2 for minutes, 1 for seconds
+    time_format = {0:0, 1:0, 2:0} 
 
-    for a_string in time_strings: # Check if input is digit
+    for i in range(len(time_strings)-1, -1, -1): # Check if input is digit
         try:
-            time_chunks.append(int(a_string))
+            time_chunks.append(int(time_strings[i]))
         except ValueError:
             return None
-    if len(time_chunks) <= 3: # Check in only hh:mm:ss
-        for i in range(3):
-            time_format[i] = time_chunk
-        
-
+    if len(time_chunks) > 3:
+        return None # time shouldn't take a day
+    for i in range(len(time_chunks)):
+        time_format[i] = time_chunks[i]
+    return time_format
 
         
 time_str = input("Enter time: ")
+time_to_countdown = parse_input(time_str)
+print(time_to_countdown)
